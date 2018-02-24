e820995389df72eb51a2086e2f859e79c81c4850
chaijuntao <ssquit@163.com>
2018-02-24 13:14:14 +0800

    message

---
+++ main/ship.py
@@ -0,0 +1,17 @@
+import pygame
+class Ship():
+
+    def __init__(self,screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+
+        #加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen.get_rect()
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
